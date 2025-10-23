63be0c4de12d396856732494b5e4d45fb81ec912
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-10-24 00:19:15 +0500

    create: frontage php - starting page

---
+++ situProject/frontpage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Sushi Sensation</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Forum&display=swap" rel="stylesheet" />
+    <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet" />
+  </head>
+  <body>
+    <section class="main-bg">
+      <nav class="header-nav">
+        <button class="header-menu-toggle">
+          <div class="header-menu-line"></div>
+          <div class="header-menu-line"></div>
+          <div class="header-menu-line"></div>
+        </button>
+        <img src="/situProject/img/Qitchen.svg" alt="Qitchen" class="header-logo" />
+        <ul class="header-menu-list">
+          <li class="header-menu-item">
+            <a href="" class="header-menu-link">Menu</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="" class="header-menu-link">About</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="" class="header-menu-link">Book a Table</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+    <script src="js/main.js"></script>
+  </body>
+</html>
